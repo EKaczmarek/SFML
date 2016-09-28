c4d7346bb8bfa61950071bef35fe06ca78cdfbf1
EKaczmarek <ela11941@wp.pl>
2016-09-28 22:48:00 +0200

    Working on random appearance of block

--- SFML_1/main.cpp
+++ SFML_1/main.cpp
@@ -1,16 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+
 int main()
 {
-	sf::RenderWindow okno(sf::VideoMode(400, 400), "2048", sf::Style::Close);
-	sf::Clock stoper;
 
+	srand(time(NULL));
+
+	float pozX, pozY = 0.0;
+	//std::vector <float,float> currentPositions;
+
+	sf::RenderWindow okno(sf::VideoMode(400, 400), "2048", sf::Style::Close);
+	//pojedynczy klocek 
 	sf::Texture textura;
 	textura.loadFromFile("klocek.png");
 
 	sf::Sprite obraz;
 	obraz.setTexture(textura);
+
+
 	while (okno.isOpen())
 	{
 		sf::Event event;
@@ -18,23 +28,35 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				okno.close();
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				if(obraz.getPosition().x != 300.0)
-					obraz.move(100, 0);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				if (obraz.getPosition().x != 0.0)
-					obraz.move(-100, 0);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				if (obraz.getPosition().y != 0.0)
-					obraz.move(0, -100);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				if (obraz.getPosition().y != 300.0)
-					obraz.move(0, 100);
-			}
 		}
+		/*if (event.type == sf::Event::KeyPressed) {
+
+			pozX = rand() % 4 + 0;
+			pozY = rand() % 4 + 0;
+			okno.draw(obraz);
+			obraz.setPosition(pozX*100, pozY*100);
+
+			currentPositions.emplace_back(pozX, pozY);
+	}*/
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			if (obraz.getPosition().x != 300.0)
+				obraz.move(0.25, 0);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			if (obraz.getPosition().x != 0.0)
+				obraz.move(-0.25, 0);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			if (obraz.getPosition().y != 0.0)
+				obraz.move(0, -0.25);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			if (obraz.getPosition().y != 300.0)
+				obraz.move(0, 0.25);
+		}
+	
+
 		okno.clear(sf::Color::Green);
 		okno.draw(obraz);
 		okno.display();
