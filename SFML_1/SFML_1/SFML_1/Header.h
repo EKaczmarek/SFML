707f497a6addeb5d897b8e9c775b52a97c57484d
EKaczmarek <e.kaczmarek01@gmail.com>
2016-10-31 16:11:33 +0100

    fixed

---
+++ SFML_1/SFML_1/SFML_1/Header.h
@@ -0,0 +1,13 @@
+#include "Block.h"
+#include "Game.h"
+#include "Font.hpp"
+
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+typedef sf::Keyboard move;
+
+#pragma once
